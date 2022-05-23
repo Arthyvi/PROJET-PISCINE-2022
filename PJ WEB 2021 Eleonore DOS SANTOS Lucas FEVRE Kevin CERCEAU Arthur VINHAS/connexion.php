73b998016733bbb5de4da2af7ebbedf6d9565ecb
Kevin-Cerceau <78725859+Kevin-Cerceau@users.noreply.github.com>
2022-05-23 16:54:57 +0200

    Login mdp

---
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/connexion.php
@@ -0,0 +1,120 @@
+<?php
+
+// Declaration des variables
+$id= isset($_POST["id"]) ? $_POST["id"] : "";
+$mdp= isset($_POST["mdp"]) ? $_POST["mdp"] : "";
+
+// Detection problème
+if($id == "")
+{
+    echo "Probleme avec la valeur de l'identifiant<br>";
+    return;
+}
+
+if($mdp == "")
+{
+    echo "Probleme avec la valeur du mot de passe<br>";
+    return;
+}
+
+
+// Connexion au serveur
+$mysqli = new mysqli("localhost","root","","projet piscine 2022");
+
+// Check connection
+if($mysqli -> connect_errno)
+{
+    echo "Failed to connect to MySQL" . $mysqli -> connect_errno;
+    exit();
+}
+else
+{
+    // Verification si Identifiant bon
+    $sql = "SELECT * FROM identifiant";
+    $BufferIDperssone = "";
+    
+
+    if($result = $mysqli->query($sql))
+    {
+        
+        if($result->num_rows >0)
+        {
+            while($row = $result->fetch_row())
+            {
+                if($row[0] == $id)
+                {
+                    $BufferIDperssone = $row[1];
+                    break;
+                }
+            }
+        }
+
+        $result->free_result();
+    }
+
+    if( $BufferIDperssone == "")
+    {
+        echo "Identifiant ou mot de passe incorect (1)<br>";
+        return;
+    }
+
+    //Definission si l'IDpersonne trouvé est : un adminisrateur (AD), un client (CL) ou un medecin (MD)
+
+    $NameTable ="";
+
+    switch(substr($BufferIDperssone,0,2))
+    {
+        case "AD": // Si c'est un administrateur...
+            $NameTable ="administrateur";
+            break;
+
+        case "CL" : // Si c'est un client...
+            $NameTable ="client";
+            break;
+
+        case "MD" : // Si c'est un Medecin...
+            $NameTable ="medecin";
+            break;
+    }
+
+
+    //// Verification si Mot de passe bon
+
+    $sql = "SELECT oth.Password FROM $NameTable oth WHERE IDpersonne='$BufferIDperssone'";
+    $Passed = false;
+
+    if($result = $mysqli->query($sql))
+    {
+        
+        if($result->num_rows >0)
+        {
+            while($row = $result->fetch_row())
+            {
+                if($row[0] == $mdp)
+                {
+                    $Passed = true;
+                    break;
+                }
+            }
+        }
+
+        $result->free_result();
+    }
+
+    if( $Passed == false)
+    {
+        echo "Identifiant ou mot de passe incorect (2)<br>";
+        return;
+    }
+    else
+    {
+        echo "Connexion etablit ".$NameTable."!!<br>";
+    }
+
+
+    // Fermeture de notre variable "$mysqli"
+    $mysqli->close();
+    
+}
+
+?>
