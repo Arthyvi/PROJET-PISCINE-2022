4b6eee0fa692877c5ca4b62c0a5d9e0dfa30536e
Kevin-Cerceau <78725859+Kevin-Cerceau@users.noreply.github.com>
2022-05-23 19:20:38 +0200

    Kevin Sign In first modif

---
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/inscription.php
@@ -0,0 +1,19 @@
+<?php
+
+// Declaration des variables
+$nom= isset($_POST["nom"]) ? $_POST["nom"] : "";
+$prenom= isset($_POST["prenom"]) ? $_POST["prenom"] : "";
+$adresse1= isset($_POST["adresse1"]) ? $_POST["adresse1"] : "";
+$adresse2= isset($_POST["adresse2"]) ? $_POST["adresse2"] : "";
+$ville= isset($_POST["ville"]) ? $_POST["ville"] : "";
+$codeP= isset($_POST["code"]) ? $_POST["code"] : "";
+$pays= isset($_POST["pays"]) ? $_POST["pays"] : "";
+$phone= isset($_POST["tel"]) ? $_POST["tel"] : "";
+$CarteVitale= isset($_POST["vitale"]) ? $_POST["vitale"] : "";
+$mail= isset($_POST["id"]) ? $_POST["id"] : "";
+$Password= isset($_POST["mdp"]) ? $_POST["mdp"] : "";
+$ConfirmedPassword= isset($_POST["mdp2"]) ? $_POST["mdp2"] : "";
+
+
+
+?>
