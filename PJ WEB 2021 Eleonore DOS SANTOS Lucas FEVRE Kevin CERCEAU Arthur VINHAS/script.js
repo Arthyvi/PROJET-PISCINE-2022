9566621ccf7b410c9b472d382b261df2908cc444
Kevin-Cerceau <78725859+Kevin-Cerceau@users.noreply.github.com>
2022-05-24 10:33:44 +0200

    Update script.js

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     $("#mdp2").keyup(function(){
 
         alert("BBB");
+        
         // Verification si la confirmation de mot de passe correspond bien au mot de passe
         
         if( $("#mdp").val() == $("#mdp2").val())
