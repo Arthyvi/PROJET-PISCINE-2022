241dddc1978e80084d806dcd7ab0735ac69954c6
Arthur <78738063+Arthyvi@users.noreply.github.com>
2022-05-24 11:22:56 +0200

    chatroom

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
@@ -1,6 +1,13 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
+    //$('#messagesent').hide();
     $('#envoyer').click(function () {
-
+        $('#messagesent').innerText = $('#messagewritten').value;
+        $('#messagesent').show();
     })
-});
+});*/
+
+function envoyer() {
+    document.getElementById('messagesent').innerText=document.getElementById('messagewritten').value;
+    document.getElementById('messagewritten').value="";
+}
 
