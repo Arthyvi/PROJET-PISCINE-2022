02d896470ca9c98cabfcfea2f543351373375838
Kevin-Cerceau <78725859+Kevin-Cerceau@users.noreply.github.com>
2022-05-24 10:36:03 +0200

    Update script.js

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
 
 
+    $("#mdp").keyup(function(){
+
+        alert("BBB");
+        
+        // Verification si la confirmation de mot de passe correspond bien au mot de passe
+        
+        if( $("#mdp").val() == $("#mdp2").val())
+        {
+            // Active le bouton "s'incrire"
+            $("#btnco").prop("disabled", false);
+            
+        }
+        else
+        {
+            // Desactive le bouton "s'incrire"
+            $("#btnco").prop("disabled", true);
+        }
+       
+    });
+
     $("#mdp2").keyup(function(){
 
         alert("BBB");
