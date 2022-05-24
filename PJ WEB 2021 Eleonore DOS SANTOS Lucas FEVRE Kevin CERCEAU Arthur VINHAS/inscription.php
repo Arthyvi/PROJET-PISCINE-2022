6474e27ee5d3139a77ec5dacfdbc7c8d38f97893
Kevin-Cerceau <78725859+Kevin-Cerceau@users.noreply.github.com>
2022-05-24 11:10:02 +0200

    Sign in client finit

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/inscription.php
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/inscription.php
@@ -46,46 +46,36 @@ else
         $result->free_result();
     }
 
-    echo $BuffID."<br>";
-
-    //Prendre les nombres à la fin et  les transformé en int
-    $NumRecupInt = intval(substr($BuffID,7)); 
-
-    // Ajouter 1 au nombre pour incrementer
-    $NumRecupInt = $NumRecupInt + 1;
-
-    echo $NumRecupInt."<br>";
+    $IDchosen = "";
 
     if($BuffID == "")
     {
-        $BuffID = "CL-00001";
+        $IDchosen = "CL-00001"; // Premier ID
     }
+    else
+    {
+        //Prendre les nombres à la fin et  les transformé en int
+        $NumRecupInt = intval(substr($BuffID,7)); 
 
-   
+        // Ajouter 1 au nombre pour incrementer
+        $NumRecupInt = $NumRecupInt + 1;
 
-
+        // ID chosen for the new client
+        $IDchosen = "CL-0000".$NumRecupInt; 
+    }
     
-    return;
+    // Transformation en INT des valeurs devant etre en int dans la BDD
+    $codeP2 =  intval($codeP);
+    $phone2 =  intval($phone);
+    $CarteVitale2 = intval($CarteVitale);
 
+    // On rajoute le client dans la table client
+    $sql = "INSERT INTO client VALUES ('$IDchosen','$nom','$prenom', '$adresse1','$adresse2','$ville',$codeP2,'$pays',$phone2,$CarteVitale2,'$Password'); ";
+    $result = $mysqli->query($sql);
 
-
-
-    $IDchosen = "";
-
-
-    $sql = "INSERT INTO client VALUES ('$IDchosen','$nom','$prenom', '$adresse1','$adresse2','$ville','$codeP','$pays','$phone','$CarteVitale','$Password'); ";
-
-
-
-$adresse1= isset($_POST["adresse1"]) ? $_POST["adresse1"] : "";
-$adresse2= isset($_POST["adresse2"]) ? $_POST["adresse2"] : "";
-$ville= isset($_POST["ville"]) ? $_POST["ville"] : "";
-$codeP= isset($_POST["code"]) ? $_POST["code"] : "";
-$pays= isset($_POST["pays"]) ? $_POST["pays"] : "";
-$phone= isset($_POST["tel"]) ? $_POST["tel"] : "";
-$CarteVitale= isset($_POST["vitale"]) ? $_POST["vitale"] : "";
-$mail= isset($_POST["id"]) ? $_POST["id"] : "";
-$Password= isset($_POST["mdp"]) ? $_POST["mdp"] : "";
+    //On rajoute l'identifiant du client dans la table identifiant
+    $sql = "INSERT INTO identifiant VALUES ('$mail','$IDchosen'); ";
+    $result = $mysqli->query($sql);
 
 
     // Fermeture de notre variable "$mysqli"
--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/script.js
@@ -3,15 +3,17 @@ $(document).ready(function () {
 
     $("#mdp").keyup(function(){
 
-        alert("BBB");
         
         // Verification si la confirmation de mot de passe correspond bien au mot de passe
         
         if( $("#mdp").val() == $("#mdp2").val())
         {
-            // Active le bouton "s'incrire"
-            $("#btnco").prop("disabled", false);
-            
+            if($("#mdp").val()  != "")
+            {
+                 // Active le bouton "s'incrire"
+                $("#btnco").prop("disabled", false);
+            }
+           
         }
         else
         {
@@ -23,14 +25,16 @@ $(document).ready(function () {
 
     $("#mdp2").keyup(function(){
 
-        alert("BBB");
         
         // Verification si la confirmation de mot de passe correspond bien au mot de passe
         
         if( $("#mdp").val() == $("#mdp2").val())
         {
-            // Active le bouton "s'incrire"
-            $("#btnco").prop("disabled", false);
+            if($("#mdp").val()  != "")
+            {
+                // Active le bouton "s'incrire"
+                $("#btnco").prop("disabled", false);
+            }
             
         }
         else
