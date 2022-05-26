4cfc461bf97895306d25491dd4fea1671cc4b5ea
lucasf1308 <78732548+lucasf1308@users.noreply.github.com>
2022-05-26 11:19:16 +0200

    EDT client pour reserver

---
+++ PP_V2/Reservation_Client.js
@@ -0,0 +1,660 @@
+$(document).ready(function(){
+    
+    var indic1=0
+    var indic2=0
+    var indic3=0
+    var indic4=0
+    var indic5=0
+    var indic6=0
+
+    var indic21=0
+    var indic22=0
+    var indic23=0
+    var indic24=0
+    var indic25=0
+    var indic26=0
+
+    var indic31=0
+    var indic32=0
+    var indic33=0
+    var indic34=0
+    var indic35=0
+    var indic36=0
+
+    var indic41=0
+    var indic42=0
+    var indic43=0
+    var indic44=0
+    var indic45=0
+    var indic46=0
+
+    var indic51=0
+    var indic52=0
+    var indic53=0
+    var indic54=0
+    var indic55=0
+    var indic56=0
+
+    var indic61=0
+    var indic62=0
+    var indic63=0
+    var indic64=0
+    var indic65=0
+    var indic66=0
+
+    var indic71=0
+    var indic72=0
+    var indic73=0
+    var indic74=0
+    var indic75=0
+    var indic76=0
+    
+
+$(".boite1-1").click(function(){
+
+
+    if (indic1==0) {
+        $(".boite1-1").css("background-color","green");
+        indic1=1}
+    
+    else {
+        $(".boite1-1").css("background-color","grey");
+        indic1=0}
+        
+    });
+
+
+
+
+$(".boite1-2").click(function(){
+
+
+    if (indic2==0) {
+        $(".boite1-2").css("background-color","green");
+        indic2=1}
+    
+    else {
+        $(".boite1-2").css("background-color","grey");
+        indic2=0}
+        
+    });
+
+
+$(".boite1-3").click(function(){
+
+
+    if (indic3==0) {
+        $(".boite1-3").css("background-color","green");
+        indic3=1}
+        
+     else {
+        $(".boite1-3").css("background-color","grey");
+        indic3=0}
+     })    
+        
+$(".boite1-4").click(function(){
+
+
+    if (indic4==0) {
+        $(".boite1-4").css("background-color","green");
+        indic4=1}
+        
+     else {
+        $(".boite1-4").css("background-color","grey");
+        indic4=0}
+           
+     })
+
+
+
+$(".boite1-5").click(function(){
+
+
+        if (indic5==0) {
+              $(".boite1-5").css("background-color","green");
+            indic5=1}
+                
+           else {
+             $(".boite1-5").css("background-color","grey");
+             indic5=0}
+           })
+
+
+$(".boite1-6").click(function(){
+
+
+        if (indic6==0) {
+              $(".boite1-6").css("background-color","green");
+              indic6=1}
+                    
+            else {
+               $(".boite1-6").css("background-color","grey");
+               indic6=0}
+               })
+           
+
+
+
+
+//**************************************************************************************************************************** */
+
+
+
+
+
+
+
+
+               $(".boite2-1").click(function(){
+
+
+                if (indic21==0) {
+                    $(".boite2-1").css("background-color","green");
+                    indic21=1}
+                
+                else {
+                    $(".boite2-1").css("background-color","grey");
+                    indic21=0}
+                    
+                });
+            
+            
+            
+            
+            $(".boite2-2").click(function(){
+            
+            
+                if (indic22==0) {
+                    $(".boite2-2").css("background-color","green");
+                    indic22=1}
+                
+                else {
+                    $(".boite2-2").css("background-color","grey");
+                    indic22=0}
+                    
+                });
+            
+            
+            $(".boite2-3").click(function(){
+            
+            
+                if (indic23==0) {
+                    $(".boite2-3").css("background-color","green");
+                    indic23=1}
+                    
+                 else {
+                    $(".boite2-3").css("background-color","grey");
+                    indic23=0}
+                 })    
+                    
+            $(".boite2-4").click(function(){
+            
+            
+                if (indic24==0) {
+                    $(".boite2-4").css("background-color","green");
+                    indic24=1}
+                    
+                 else {
+                    $(".boite2-4").css("background-color","grey");
+                    indic24=0}
+                       
+                 })
+            
+            
+            
+            $(".boite2-5").click(function(){
+            
+            
+                    if (indic25==0) {
+                          $(".boite2-5").css("background-color","green");
+                        indic25=1}
+                            
+                       else {
+                         $(".boite2-5").css("background-color","grey");
+                         indic25=0}
+                       })
+            
+            
+            $(".boite2-6").click(function(){
+            
+            
+                    if (indic26==0) {
+                          $(".boite2-6").css("background-color","green");
+                          indic26=1}
+                                
+                        else {
+                           $(".boite2-6").css("background-color","grey");
+                           indic26=0}
+                           })
+
+
+//***************************************************************************************************************************************** */
+
+
+$(".boite3-1").click(function(){
+
+
+    if (indic31==0) {
+        $(".boite3-1").css("background-color","green");
+        indic31=1}
+    
+    else {
+        $(".boite3-1").css("background-color","grey");
+        indic31=0}
+        
+    });
+
+
+
+
+$(".boite3-2").click(function(){
+
+
+    if (indic32==0) {
+        $(".boite3-2").css("background-color","green");
+        indic32=1}
+    
+    else {
+        $(".boite3-2").css("background-color","grey");
+        indic32=0}
+        
+    });
+
+
+$(".boite3-3").click(function(){
+
+
+    if (indic33==0) {
+        $(".boite3-3").css("background-color","green");
+        indic33=1}
+        
+     else {
+        $(".boite3-3").css("background-color","grey");
+        indic33=0}
+     })    
+        
+$(".boite3-4").click(function(){
+
+
+    if (indic34==0) {
+        $(".boite3-4").css("background-color","green");
+        indic34=1}
+        
+     else {
+        $(".boite3-4").css("background-color","grey");
+        indic34=0}
+           
+     })
+
+
+
+$(".boite3-5").click(function(){
+
+
+        if (indic35==0) {
+              $(".boite3-5").css("background-color","green");
+            indic35=1}
+                
+           else {
+             $(".boite3-5").css("background-color","grey");
+             indic35=0}
+           })
+
+
+$(".boite3-6").click(function(){
+
+
+        if (indic36==0) {
+              $(".boite3-6").css("background-color","green");
+              indic36=1}
+                    
+            else {
+               $(".boite3-6").css("background-color","grey");
+               indic36=0}
+               })
+
+
+
+//***************************************************************************************************************************************** */
+
+
+$(".boite4-1").click(function(){
+
+
+    if (indic41==0) {
+        $(".boite4-1").css("background-color","green");
+        indic41=1}
+    
+    else {
+        $(".boite4-1").css("background-color","grey");
+        indic41=0}
+        
+    });
+
+
+
+
+$(".boite4-2").click(function(){
+
+
+    if (indic42==0) {
+        $(".boite4-2").css("background-color","green");
+        indic42=1}
+    
+    else {
+        $(".boite4-2").css("background-color","grey");
+        indic42=0}
+        
+    });
+
+
+$(".boite4-3").click(function(){
+
+
+    if (indic43==0) {
+        $(".boite4-3").css("background-color","green");
+        indic43=1}
+        
+     else {
+        $(".boite4-3").css("background-color","grey");
+        indic43=0}
+     })    
+        
+$(".boite4-4").click(function(){
+
+
+    if (indic44==0) {
+        $(".boite4-4").css("background-color","green");
+        indic44=1}
+        
+     else {
+        $(".boite4-4").css("background-color","grey");
+        indic44=0}
+           
+     })
+
+
+
+$(".boite4-5").click(function(){
+
+
+        if (indic45==0) {
+              $(".boite4-5").css("background-color","green");
+            indic45=1}
+                
+           else {
+             $(".boite4-5").css("background-color","grey");
+             indic45=0}
+           })
+
+
+$(".boite4-6").click(function(){
+
+
+        if (indic46==0) {
+              $(".boite4-6").css("background-color","green");
+              indic46=1}
+                    
+            else {
+               $(".boite4-6").css("background-color","grey");
+               indic46=0}
+               })
+
+
+
+//*****************************************************************************************************************************************
+
+$(".boite5-1").click(function(){
+
+
+    if (indic51==0) {
+        $(".boite5-1").css("background-color","green");
+        indic51=1}
+    
+    else {
+        $(".boite5-1").css("background-color","grey");
+        indic51=0}
+        
+    });
+
+
+
+
+$(".boite5-2").click(function(){
+
+
+    if (indic52==0) {
+        $(".boite5-2").css("background-color","green");
+        indic52=1}
+    
+    else {
+        $(".boite5-2").css("background-color","grey");
+        indic52=0}
+        
+    });
+
+
+$(".boite5-3").click(function(){
+
+
+    if (indic53==0) {
+        $(".boite5-3").css("background-color","green");
+        indic53=1}
+        
+     else {
+        $(".boite5-3").css("background-color","grey");
+        indic53=0}
+     })    
+        
+$(".boite5-4").click(function(){
+
+
+    if (indic54==0) {
+        $(".boite5-4").css("background-color","green");
+        indic54=1}
+        
+     else {
+        $(".boite5-4").css("background-color","grey");
+        indic54=0}
+           
+     })
+
+
+
+$(".boite5-5").click(function(){
+
+
+        if (indic55==0) {
+              $(".boite5-5").css("background-color","green");
+            indic55=1}
+                
+           else {
+             $(".boite5-5").css("background-color","grey");
+             indic55=0}
+           })
+
+
+$(".boite5-6").click(function(){
+
+
+        if (indic56==0) {
+              $(".boite5-6").css("background-color","green");
+              indic56=1}
+                    
+            else {
+               $(".boite5-6").css("background-color","grey");
+               indic56=0}
+               })
+
+//*****************************************************************************************************************************************
+
+
+$(".boite6-1").click(function(){
+
+
+    if (indic61==0) {
+        $(".boite6-1").css("background-color","green");
+        indic61=1}
+    
+    else {
+        $(".boite6-1").css("background-color","grey");
+        indic61=0}
+        
+    });
+
+
+
+
+$(".boite6-2").click(function(){
+
+
+    if (indic62==0) {
+        $(".boite6-2").css("background-color","green");
+        indic62=1}
+    
+    else {
+        $(".boite6-2").css("background-color","grey");
+        indic62=0}
+        
+    });
+
+
+$(".boite6-3").click(function(){
+
+
+    if (indic63==0) {
+        $(".boite6-3").css("background-color","green");
+        indic63=1}
+        
+     else {
+        $(".boite6-3").css("background-color","grey");
+        indic63=0}
+     })    
+        
+$(".boite6-4").click(function(){
+
+
+    if (indic64==0) {
+        $(".boite6-4").css("background-color","green");
+        indic64=1}
+        
+     else {
+        $(".boite6-4").css("background-color","grey");
+        indic64=0}  
+     })
+
+
+
+$(".boite6-5").click(function(){
+
+
+        if (indic65==0) {
+              $(".boite6-5").css("background-color","green");
+            indic65=1}
+                
+           else {
+             $(".boite6-5").css("background-color","grey");
+             indic65=0}
+           })
+
+
+$(".boite6-6").click(function(){
+
+
+        if (indic66==0) {
+              $(".boite6-6").css("background-color","green");
+              indic66=1}
+                    
+            else {
+               $(".boite6-6").css("background-color","grey");
+               indic66=0}
+               })
+
+
+//*****************************************************************************************************************************************
+
+
+$(".boite7-1").click(function(){
+
+
+    if (indic71==0) {
+        $(".boite7-1").css("background-color","green");
+        indic71=1}
+    
+    else {
+        $(".boite7-1").css("background-color","grey");
+        indic71=0}
+        
+    });
+
+
+
+
+$(".boite7-2").click(function(){
+
+
+    if (indic72==0) {
+        $(".boite7-2").css("background-color","green");
+        indic72=1}
+    
+    else {
+        $(".boite7-2").css("background-color","grey");
+        indic72=0}
+        
+    });
+
+
+$(".boite7-3").click(function(){
+
+
+    if (indic73==0) {
+        $(".boite7-3").css("background-color","green");
+        indic73=1}
+        
+     else {
+        $(".boite7-3").css("background-color","grey");
+        indic73=0}
+     })    
+        
+$(".boite7-4").click(function(){
+
+
+    if (indic74==0) {
+        $(".boite7-4").css("background-color","green");
+        indic74=1}
+        
+     else {
+        $(".boite7-4").css("background-color","grey");
+        indic74=0}  
+     })
+
+
+
+$(".boite7-5").click(function(){
+
+
+        if (indic75==0) {
+              $(".boite7-5").css("background-color","green");
+            indic75=1}
+                
+           else {
+             $(".boite7-5").css("background-color","grey");
+             indic75=0}
+           })
+
+
+$(".boite7-6").click(function(){
+
+
+        if (indic76==0) {
+              $(".boite7-6").css("background-color","green");
+              indic76=1}
+                    
+            else {
+               $(".boite7-6").css("background-color","grey");
+               indic76=0}
+               })
+
+
+
+});
