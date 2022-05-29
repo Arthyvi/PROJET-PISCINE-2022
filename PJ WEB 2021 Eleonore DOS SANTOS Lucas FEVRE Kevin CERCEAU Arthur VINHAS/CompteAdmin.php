1e6396d9d0dc1c1b7a317e4eea9c25447e271dd4
Arthyvi <78738063+Arthyvi@users.noreply.github.com>
2022-05-29 22:15:25 +0200

    Update CompteAdmin.php

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/CompteAdmin.php
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/CompteAdmin.php
@@ -3,7 +3,7 @@
     session_start();
 
      // Connexion au serveur
-     $mysqli = new mysqli("localhost:3309","root","","projet piscine 2022");
+     $mysqli = new mysqli("localhost:3306","root","","projet piscine 2022");
 
      // Check connection
      if($mysqli -> connect_errno)
