e1d27f914ad48c0d0bf28a92278e685d0f5ce59b
Arthyvi <78738063+Arthyvi@users.noreply.github.com>
2022-05-29 17:52:02 +0200

    Update home.php

--- PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/home.php
+++ PJ WEB 2021 Eleonore DOS SANTOS Lucas FEVRE Kevin CERCEAU Arthur VINHAS/home.php
@@ -43,7 +43,7 @@ $_SESSION["SelectedDoc"]="";
 <?php
 // Connexion au serveur
 
-$mysqli = new mysqli("localhost:3309", "root", "", "projet piscine 2022");
+$mysqli = new mysqli("localhost:3306", "root", "", "projet piscine 2022");
 
 // Check connection
 if ($mysqli->connect_errno) {
